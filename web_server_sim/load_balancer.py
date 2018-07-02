d7aea300329a87b98c70d4a2b0643aed32a4cf5e
Sandeep Thukral <sadeep.thukral@insided.com>
2018-07-02 14:09:44 +0200

    load balancer with itertools

--- web_server_sim/load_balancer.py
+++ web_server_sim/load_balancer.py
@@ -1,11 +1,18 @@
 ### SERVER NAMES
 SERVERS = ['APP1', 'APP2', 'APP3']
-count = -1
+# count = -1
+
+# def get_server():
+#   global count
+#   count = count + 1
+#   return SERVERS[count % len(SERVERS)]
+
+import itertools
+cycle = itertools.cycle(SERVERS)
 
 def get_server():
-  global count
-  count = count + 1
-  return SERVERS[count % len(SERVERS)]
+  global cycle
+  return cycle.next()
 
 ### testing the function
 if __name__ == '__main__':
